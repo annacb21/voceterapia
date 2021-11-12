a431b723c5ed09e65bd2a49dc4e2f5a92c53d6d7
annacb21 <annacb21@gmail.com>
2021-11-12 16:46:34 +0100

    aggiunto footer a index.php

--- index.php
+++ index.php
@@ -20,6 +20,7 @@
     <!-- UP BUTTON -->
 
     <!-- FOOTER -->
+    <?php include(TEMPLATE_FRONT . DS . "footer.php"); ?>
 
     <script src="https://kit.fontawesome.com/90922573b7.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
