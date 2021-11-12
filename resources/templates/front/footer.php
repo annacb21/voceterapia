6fb469699b5f6bb71a62f1f8ba454c0e354579b1
annacb21 <annacb21@gmail.com>
2021-11-12 16:45:38 +0100

    fatto footer

---
+++ resources/templates/front/footer.php
@@ -0,0 +1,56 @@
+<footer class="bg-dark text-white">
+    <div class="row">
+        <div class="col-lg-4">
+            <h4>Navigazione</h4>
+            <ul class="nav flex-column">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="formazione.php">Formazione</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="voceterapia.php">Voceterapia</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="canto.php">Canto</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="recensioni.php">Recensioni</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="news.php">News</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contatti.php">Contatti</a>
+                </li>
+            </ul>
+        </div>
+        <div class="col-lg-4">
+            <h4>Contatti</h4>
+            <p><i class="fas fa-at"></i>elisa.fortunati@virgilio.it</p>
+            <a href="#"><span><i class="fab fa-facebook"></i>Elisa Fortunati</span></a>
+            <a href="#"><span><i class="fab fa-instagram"></i>@elisafortunati</span></a>
+        </div>
+        <div class="col-lg-4">
+            <div class="row">
+                <div>
+                    <h4>Dove trovarmi</h4>
+                    <p>Verona (VR), zona centro storico</p>
+                </div>
+                <div>
+                    <h4>Sito personale</h4>
+                    <p>www.elisafortunati.it</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6">
+            <p>P.IVA 09304481006 <span>Privacy Policy</span> <span>Cookie Policy</span> ©Copiryght 2021</p>
+        </div>
+        <div class="col-lg-6">
+            <a role="button" class="btn btn-secondary" href="login.php"><i class="fas fa-key"></i></a>
+        </div>
+    </div>
+</footer>
