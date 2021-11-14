009a5e8de9e972cf4c8b76983f26d2fcbed3036d
annacb21 <annacb21@gmail.com>
2021-11-14 18:05:58 +0100

    fatta pagina formazione.php

--- formazione.php
+++ formazione.php
@@ -17,6 +17,52 @@
     <!-- NAVBAR -->
     <?php include(TEMPLATE_FRONT . DS . "navbar.php"); ?>
 
+    <!-- MAIN CONTENT -->
+    <div class="row">
+        <div class="col-lg-7">
+            <div class="row">
+                <div>
+                    <h1>Formazione</h1>
+                </div>
+                <div>
+                    <p>Dopo il conseguimento del Diploma di Canto con 10 e lode, ho conseguito la laurea in Filosofia con 110 e lode.
+                    Ho pubblicato un articolo relativo alla tesi di laurea “Sul Compendium musicae” di Cartesio nella Rivista Musicologica “Quaderni di musicologia” ed ho vinto il concorso come Coordinatore dei Servizi Musicali della Fondazione Arena.
+                    Ho Iniziato la carriera della solista 20 anni fa affiancando l’attività artistica a quella didattica.
+                    Appassionata da sempre di canto e voce, ho seguito corsi con i migliori maestri ed esperti della tecnica vocale, con particolare attenzione alle seguenti problematiche: respirazione, postura, collegamento del fiato col suono, ginnastica facciale, ortoepia, articolazione sillabica, ginnastica facciale e dizione.
+                    Ho tenuto quindi corsi di canto, respirazione e percezione corporea ed, in stretta collaborazione con psicologi e specialisti di disturbi infantili e adolescenziali, ho effettuato con successo attività di supporto a problemi di balbuzie e/o difficoltà emotiva, utilizzando la musica, tecniche di respirazione e vocali.
+                    Mi sono anche dedicata all’impostazione, rieducazione vocale e respiratoria, di adulti ed adolescenti con problematiche nel meccanismo pneumofonico e conseguenti difficoltà di comunicazione.
+                    Ho tenuto corsi di impostazione vocale e canto (lirico e leggero) in scuole private, per studenti del Conservatorio e corsi di musica nella scuola pubblica sia come esperta esterna che come docente interna. 
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <img src="images/pripia11.jpg" alt="foto elisa fortunati">
+        </div>
+    </div>
+    <div class="row">
+    <div class="col-lg-4">
+            <img src="images/pripia9.jpg" alt="foto elisa fortunati">
+        </div>
+        <div class="col-lg-7">
+            <div class="row">
+                <div>
+                    <h2>Curriculum Vitae</h2>
+                </div>
+                <div>
+                    <p>Ho cantato per la Fondazione Arena di Verona in Rigoletto di Verdi al Teatro Filarmonico e in Didone ed Enea di Purcell al Teatro Ristori. Per il Maggio Musicale Fiorentino ho lavorato nei due allestimenti di Die Frau ohne Schatten e Der Rosenkavalier di Strauss, entrambi diretti da Zubin Mehta.  In diversi teatri di tradizione mi sono  esibita in importanti titoli del mio repertorio (Suor Angelica di Puccini, La traviata di Verdi, I quatro rusteghi di Wolf-Ferrari, ecc..).
+                    Ho vinto,  con menzione d’onore, il concorso Rome Festival per il ruolo di Hänsel in Hänsel und Gretel di Humperdinck, prima classificata al Tirindelli, Concorso Internazionale della Romanza da salotto (contestualmente premiata anche nella sezione romanze celebri).
+                    Sono stata protagonista della prima assoluta di Antinesca (nel ruolo del titolo) in collaborazione con la Biennale di Venezia, nella prima italiana de La regina delle nevi al Teatro Sociale di Vicenza e nella prima esecuzione in tempi moderni di Adelaide di Borgogna al Sociale di Rovigo.
+                    Come concertista ho al mio attivo numeroso  concerti di musica da camera con  diverse formazioni musicali; mi sono inoltre dedicata anche al repertorio sacro (in collaborazione con l’Accademia Filarmonica esegue il Requiem di Mozart, Lauda Sion di Mendelssohn, Stabat Mater di Rossini).
+                    </p>
+                </div>
+                <div>
+                    <a href="http://www.elisafortunati.it/home.html" role="button" class="btn btn-primary" aria-label="sito personale">Sito personale <span><i class="fas fa-arrow-right"></i></span></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <!-- UP BUTTON -->
     <button type="button" class="btn rounded-circle shadow btn-lg" id="upBtn" onclick="backToTop()"><i class="fas fa-chevron-up"></i></button>
 
