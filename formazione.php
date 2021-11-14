6b7c23dfd9168849dd17b83fc4e250eea37781e6
annacb21 <annacb21@gmail.com>
2021-11-14 17:53:56 +0100

    creata pagina formazione

---
+++ formazione.php
@@ -0,0 +1,31 @@
+<?php require_once("resources/config.php"); ?>
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <title>Formazione | Voceterapia</title>
+    <meta charset="utf-8">
+    <meta name="description" content=""/>
+    <meta name="keywords" content="voceterpia, insegnate di voceterapia, elisa fortunati, verona, lezioni di canto, insegnante di canto, canto, logopedista, terapista, recitazione, dizione, riabilitazione, rieducazione, psicologa, balbuzie, tecnica vocale, postura, benessere, rilassamento, anti stress"/>
+    <meta name="author" content="Anna Cisotto Bertocco"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+    
+    <!-- NAVBAR -->
+    <?php include(TEMPLATE_FRONT . DS . "navbar.php"); ?>
+
+    <!-- UP BUTTON -->
+    <button type="button" class="btn rounded-circle shadow btn-lg" id="upBtn" onclick="backToTop()"><i class="fas fa-chevron-up"></i></button>
+
+    <!-- FOOTER -->
+    <?php include(TEMPLATE_FRONT . DS . "footer.php"); ?>
+
+    <script src="https://kit.fontawesome.com/90922573b7.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    <script src="js/scrollToTop.js"></script>
+</body>
+</html>
