5d3bf653688b07c9d9c6032fb29f6125052727cc
annacb21 <annacb21@gmail.com>
2021-11-14 17:09:54 +0100

    creata classe recensione.php

---
+++ resources/classes/recensione.php
@@ -0,0 +1,53 @@
+<?php
+
+class Recensione {
+
+    private $id;
+    private $autore;
+    private $foto_autore;
+    private $titolo;
+    private $testo;
+    private $punteggio;
+    private $data;
+
+    public function __construct($i, $a, $fa, $t, $te, $p, $d) {
+        $this->id = $i;
+        $this->autore = $a;
+        $this->foto_autore = $fa;
+        $this->titolo = $t;
+        $this->testo = $te;
+        $this->punteggio = $p;
+        $this->data = $d;
+    }
+
+    public function get_id() {
+        return $this->id;
+    }
+
+    public function get_autore() {
+        return $this->autore != null ? $this->autore : "anonimo";
+    }
+
+    public function get_foto_autore() {
+        return $this->foto_autore != null ? $this->foto_autore : "nullprofile.jpg";
+    }
+
+    public function get_title() {
+        return $this->titolo;
+    }
+
+    public function get_punteggio() {
+        return $this->punteggio;
+    }
+
+    public function get_data() {
+        return $this->data;
+    }
+
+    public function get_testo() {
+        return $this->testo;
+    }
+
+}
+
+?>
