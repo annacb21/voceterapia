62d3ff19f9c1765746bb28b14000b60965ec3d9c
annacb21 <annacb21@gmail.com>
2021-11-19 11:05:05 +0100

    create funzioni utility

--- resources/functions.php
+++ resources/functions.php
@@ -3,6 +3,39 @@
 require_once("classes/recensione.php");
 require_once("database.php");
 
+//*************************** SYSTEM FUNCTIONS ****************************
+// redirect to a page
+function redirect($location) {
+    return header("Location: $location ");
+}
+
+// create an alert message
+function set_message($msg, $alert_type) {
+    if(!empty($msg)) {
+        $_SESSION['message'] = $msg;
+        $_SESSION['alert'] = $alert_type;
+    }
+    else {
+        $msg = "";
+        $alert_type = "";
+    }
+}
+
+// display an alert message
+function display_message() {
+
+if(isset($_SESSION['message']) && isset($_SESSION['alert'])) {
+    
+$alert = <<<DELIMETER
+<div class="alert {$_SESSION['alert']} w-50 text-center mx-auto" role="alert">
+    {$_SESSION['message']}
+</div>
+DELIMETER;
+echo $alert;
+unset($_SESSION['message']);
+}
+
+}
 
 
 ?>
