e2b056141c26a26dfdb116a248a98fb61ff502df
annacb21 <annacb21@gmail.com>
2021-11-26 09:57:59 +0100

    spostato css admin in file a parte

--- admin/admin.php
+++ admin/admin.php
@@ -17,7 +17,7 @@ $row = fetch_array($query);
     <meta name="keywords" content="voceterpia, insegnate di voceterapia, elisa fortunati, verona, lezioni di canto, insegnante di canto, canto, logopedista, terapista, recitazione, dizione, riabilitazione, rieducazione, psicologa, balbuzie, tecnica vocale, postura, benessere, rilassamento, anti stress"/>
     <meta name="author" content="Anna Cisotto Bertocco"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="../styles.css">
+    <link rel="stylesheet" href="admin-style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
