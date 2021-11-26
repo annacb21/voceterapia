4f88e47cef9b37b91bd6c69e35f1b5480b3a2c1d
annacb21 <annacb21@gmail.com>
2021-11-26 15:45:01 +0100

    fatto css footer

--- resources/templates/front/footer.php
+++ resources/templates/front/footer.php
@@ -1,56 +1,73 @@
-<footer class="bg-dark text-white">
-    <div class="row">
-        <div class="col-lg-4">
-            <h3>Navigazione</h3>
-            <ul class="nav flex-column">
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="formazione.php">Formazione</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="voceterapia.php">Voceterapia</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="canto.php">Canto</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="recensioni.php">Recensioni</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="news.php">News</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="contatti.php">Contatti</a>
-                </li>
-            </ul>
-        </div>
-        <div class="col-lg-4">
-            <h3>Contatti</h3>
-            <p><i class="fas fa-at"></i> <span>elisa.fortunati@virgilio.it</span></p>
-            <a href="#"><i class="fab fa-facebook"></i> <span>Elisa Fortunati</span></a>
-            <a href="#"><i class="fab fa-instagram"></i> <span>@elisafortunati</span></a>
-        </div>
-        <div class="col-lg-4">
-            <div class="row">
-                <div>
-                    <h3>Dove trovarmi</h3>
+<footer>
+    <div class="content-padding">
+        <div class="row pt-5 justify-content-between">
+            <div class="col-lg-4">
+                <h3 class="pb-3 h4">Navigazione</h3>
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="formazione.php">Formazione</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="voceterapia.php">Voceterapia</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="canto.php">Canto</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="recensioni.php">Recensioni</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="news.php">News</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contatti.php">Contatti</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-lg-4">
+                <h3 class="pb-3 h4">Contatti</h3>
+                <div class="row align-items-center pb-3">
+                    <div class="col-lg-1">
+                        <i class="fas fa-at"></i>
+                    </div>
+                    <div class="col-lg-11">
+                        <p class="card-text">elisa.fortunati@virgilio.it</p>
+                    </div>
+                </div>
+                <div class="row align-items-center pb-3">
+                    <div class="col-lg-1">
+                        <i class="fab fa-facebook"></i>
+                    </div>
+                    <div class="col-lg-11">
+                        <a href="#" class="card-text">Elisa Fortunati</a>
+                    </div>
+                </div>
+                <div class="row align-items-center pb-3">
+                    <div class="col-lg-1">
+                        <i class="fab fa-instagram"></i>
+                    </div>
+                    <div class="col-lg-11">
+                        <a href="#" class="card-text">@elisafortunati</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="pb-5">
+                    <h3 class="pb-3 h4">Dove trovarmi</h3>
                     <p>Verona (VR), zona centro storico</p>
                 </div>
                 <div>
-                    <h3>Sito personale</h3>
+                    <h3 class="pb-3 h4">Sito personale</h3>
                     <a href="http://www.elisafortunati.it/home.html">www.elisafortunati.it</a>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-lg-6">
-            <p>P.IVA 09304481006 <span>Privacy Policy</span> <span>Cookie Policy</span> ©Copiryght 2021</p>
-        </div>
-        <div class="col-lg-6">
-            <a role="button" class="btn btn-secondary" href="login.php"><i class="fas fa-key"></i></a>
+        <div class="py-4">
+            <p class="text-center text-muted">P.IVA 09304481006 <span>Privacy Policy</span> <span>Cookie Policy</span> ©Copiryght 2021</p>
+            <a role="button" class="btn btn-outline-secondary text-muted" href="login.php"><i class="fas fa-key"></i></a>
         </div>
     </div>
 </footer>
