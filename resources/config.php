06bb986aa35c43c2cdc89cc051a6429d4f22ca47
annacb21 <annacb21@gmail.com>
2021-12-09 10:22:38 +0100

    modificati dati databse

--- resources/config.php
+++ resources/config.php
@@ -13,16 +13,13 @@ defined("TEMPLATE_BACK") ? null : define("TEMPLATE_BACK", __DIR__ . DS . "templa
 
 defined("IMAGES") ? null : define("IMAGES", __DIR__ . DS . "../images");
 
-defined("UPLOADS") ? null : define("UPLOADS", __DIR__ . DS . "uploads");
-
-
 defined("DB_HOST") ? null : define("DB_HOST", "localhost");
 
-defined("DB_USER") ? null : define("DB_USER", "root");
+defined("DB_USER") ? null : define("DB_USER", "u529235838_voceterapia");
 
-defined("DB_PASS") ? null : define("DB_PASS", "");
+defined("DB_PASS") ? null : define("DB_PASS", "Poppocorno96");
 
-defined("DB_NAME") ? null : define("DB_NAME", "voceterapia_db");
+defined("DB_NAME") ? null : define("DB_NAME", "u529235838_voceterapia_db");
 
 
 $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
